bb83fa78a2e0f9c4ed4a2b219d261ba6816698e6
matthew-dean-brown <mattrewbrown@gmail.com>
2025-08-26 12:12:21 +0200

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <navbar-comp></navbar-comp>
   <router-view/>
 </template>
 
+<script>
+import NavbarComp from './components/NavbarComp.vue';
+
+export default{
+  components:{
+    NavbarComp
+  }
+}
+</script>
+
 <style>
+*{
+  box-sizing: 0;
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Poppins, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+
+
 </style>
---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav id="navbar">
+    <div class="nav-container">
+      <!-- Left side -->
+      <div id="logo">
+        Skill Swap
+      </div>
+
+      <!-- Right side -->
+      <ul class="nav-links">
+        <router-link to="/">Dashboard</router-link> |
+        <router-link to="/about">Micro lessons</router-link> |
+        <router-link to="/about">Profile</router-link> |
+        <router-link to="/about">Login/Register</router-link> 
+
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+/* NAV BAR STYLES */
+#navbar {
+  background-color: #5a35f3; /* same purple as screenshot */
+  padding: 15px 30px;
+}
+
+/* Container flex */
+.nav-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* Logo text */
+#logo {
+  color: white;
+  font-size: 16px;
+  font-weight: 500;
+}
+
+/* Nav links */
+.nav-links {
+  list-style: none;
+  display: flex;
+  gap: 40px;
+  margin: 0;
+  padding: 0;
+}
+
+.nav-links li a, a{
+  text-decoration: none;
+  color: white;
+  font-size: 14px;
+  font-weight:400;
+}
+
+nav a.router-link-exact-active {
+  text-decoration: underline;
+}
+</style>
